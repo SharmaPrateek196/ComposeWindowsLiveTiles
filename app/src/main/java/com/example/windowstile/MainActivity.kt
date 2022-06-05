f5ef32881badd2b7266d0e37ce49a41a0cc7724c
Prateek Sharma <prateeksharma@Prateeks-MacBook-Pro.local>
2022-06-06 02:50:41 +0530

    feat: live tiles using handler

--- app/src/main/java/com/example/windowstile/MainActivity.kt
+++ app/src/main/java/com/example/windowstile/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.windowstile
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.service.quicksettings.Tile
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -25,6 +27,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import coil.compose.AsyncImage
 import com.example.windowstile.ui.theme.WindowsTileTheme
 
@@ -34,25 +37,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WindowsTileTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
 
-//                    LazyVerticalGrid(
-//                        cells = GridCells.Fixed(3),
-//                        modifier = Modifier.padding(8.dp),
-//                    ) {
-//                        items(5){
-//                            WindowTile()
-//                        }
-//                    }
-
-                    WindowTile(images = listOf(
-                        "https://demo.sirv.com/nuphar.jpg?w=400",
-                        "https://demo.sirv.com/looks.jpg?w=400"
-                    ))
+                    LazyVerticalGrid(
+                        cells = GridCells.Fixed(3),
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        getImagesLists().let { list ->
+                            list.forEach {
+                                item {
+                                    WindowTile(images = it.list, it.delay)
+                                }
+                            }
+                        }
+                    }
 
                 }
             }
@@ -62,42 +64,178 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun WindowTile(images: List<String>) {
+fun WindowTile(images: List<String>, delay: Long) {
 
     var currentItemIndex by remember { mutableStateOf(0) }
     val size = images.size
 
-    Column(modifier = Modifier) {
-        Surface(
-            modifier = Modifier
-                .padding(2.dp)
-                .size(120.dp)
-        ) {
-            AnimatedContent(
-                targetState = currentItemIndex,
-                transitionSpec = {
-                    slideInVertically(animationSpec = TweenSpec(1000, easing = FastOutSlowInEasing), initialOffsetY =  { height -> height })
-                        .with(slideOutVertically(animationSpec = TweenSpec(1000, easing = FastOutSlowInEasing), targetOffsetY = { height -> -height}))
-                }
-            ) { index ->
-                AsyncImage(
-                    model = images[index%size],
-                    contentScale = ContentScale.FillBounds,
-                    contentDescription = "null"
-                )
+    val handler = Handler(Looper.getMainLooper())
+
+    handler.post(
+        object : Runnable {
+            override fun run() {
+                currentItemIndex++
+                if(delay>0) handler.postDelayed(this, delay)
             }
         }
+    )
 
-        Button(onClick = { currentItemIndex++ }) {
-            Text(text = "change img")
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .size(120.dp)
+    ) {
+        AnimatedContent(
+            targetState = currentItemIndex,
+            transitionSpec = {
+                slideInVertically(animationSpec = TweenSpec(1800, easing = FastOutSlowInEasing), initialOffsetY =  { height -> height })
+                    .with(slideOutVertically(animationSpec = TweenSpec(1800, easing = FastOutSlowInEasing), targetOffsetY = { height -> -height}))
+            }
+        ) { index ->
+            AsyncImage(
+                model = images[index%size],
+                contentScale = ContentScale.FillBounds,
+                contentDescription = "null"
+            )
         }
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WindowsTileTheme {
-        
-    }
+fun getImagesLists(): List<TileItem> {
+    return listOf(
+        TileItem(
+            listOf(
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+                "https://images.pexels.com/photos/7475822/pexels-photo-7475822.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427"
+            ),
+            3500
+        ),
+        TileItem(
+            listOf(
+                "https://images.pexels.com/photos/8378108/pexels-photo-8378108.jpeg?cs=srgb&dl=pexels-raquel-8378108.jpg&fm=jpg&w=640&h=960",
+                "https://images.pexels.com/photos/11625335/pexels-photo-11625335.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427"
+            ),
+            0
+        ),
+        TileItem(
+            listOf(
+                "https://images.pexels.com/photos/4792719/pexels-photo-4792719.jpeg?cs=srgb&dl=pexels-anete-lusina-4792719.jpg&fm=jpg&w=640&h=427",
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+            ),
+            4500
+        ),
+        TileItem(
+            listOf(
+                "https://images.pexels.com/photos/4631086/pexels-photo-4631086.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427",
+                "https://images.pexels.com/photos/11625335/pexels-photo-11625335.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427"
+            ),
+            0
+        ),TileItem(
+            listOf(
+                "https://images.pexels.com/photos/7475822/pexels-photo-7475822.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427",
+                "https://images.pexels.com/photos/11625335/pexels-photo-11625335.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427"
+            ),
+            0
+        ),
+        TileItem(
+            listOf(
+                "https://images.pexels.com/photos/10584840/pexels-photo-10584840.jpeg?cs=srgb&dl=pexels-adam-cole-10584840.jpg&fm=jpg&w=640&h=427",
+                "https://images.pexels.com/photos/11625335/pexels-photo-11625335.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427"
+            ),
+            9000
+        ),TileItem(
+            listOf(
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+                "https://images.pexels.com/photos/8378108/pexels-photo-8378108.jpeg?cs=srgb&dl=pexels-raquel-8378108.jpg&fm=jpg&w=640&h=960"
+            ),
+            0
+        ),
+        TileItem(
+            listOf(
+                "https://images.pexels.com/photos/4631086/pexels-photo-4631086.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427",
+                "https://images.pexels.com/photos/8378108/pexels-photo-8378108.jpeg?cs=srgb&dl=pexels-raquel-8378108.jpg&fm=jpg&w=640&h=960"
+            ),
+            5500
+        ),
+        TileItem(
+            listOf(
+                "https://images.pexels.com/photos/4631086/pexels-photo-4631086.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427",
+                "https://images.pexels.com/photos/11625335/pexels-photo-11625335.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427"
+            ),
+            0
+        ),
+        TileItem(
+            listOf(
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+                "https://images.pexels.com/photos/4631086/pexels-photo-4631086.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427"
+            ),
+            0
+        ),
+        TileItem(
+            listOf(
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+                "https://images.pexels.com/photos/10584840/pexels-photo-10584840.jpeg?cs=srgb&dl=pexels-adam-cole-10584840.jpg&fm=jpg&w=640&h=427"
+            ),
+            0
+        ),
+        TileItem(
+            listOf(
+                "https://images.pexels.com/photos/4631086/pexels-photo-4631086.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427",
+                "https://images.pexels.com/photos/11625335/pexels-photo-11625335.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427"
+            ),
+            6500
+        ),TileItem(
+            listOf(
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+                "https://images.pexels.com/photos/8378108/pexels-photo-8378108.jpeg?cs=srgb&dl=pexels-raquel-8378108.jpg&fm=jpg&w=640&h=960"
+            ),
+            0
+        ),TileItem(
+            listOf(
+                "https://images.pexels.com/photos/4631086/pexels-photo-4631086.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427",
+                "https://images.pexels.com/photos/11625335/pexels-photo-11625335.jpeg?cs=srgb&dl=pexels-printexstar-11625335.jpg&fm=jpg&w=640&h=427"
+            ),
+            0
+        ),
+        TileItem(
+            listOf(
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+                "https://images.pexels.com/photos/10584840/pexels-photo-10584840.jpeg?cs=srgb&dl=pexels-adam-cole-10584840.jpg&fm=jpg&w=640&h=427"
+            ),
+            7200
+        ),
+        TileItem(
+            listOf(
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+                "https://images.pexels.com/photos/8378108/pexels-photo-8378108.jpeg?cs=srgb&dl=pexels-raquel-8378108.jpg&fm=jpg&w=640&h=960"
+            ),
+            0
+        ),
+        TileItem(
+            listOf(
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+                "https://images.pexels.com/photos/8378108/pexels-photo-8378108.jpeg?cs=srgb&dl=pexels-raquel-8378108.jpg&fm=jpg&w=640&h=960"
+            ),
+            0
+        ),
+        TileItem(
+            listOf(
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+                "https://images.pexels.com/photos/8378108/pexels-photo-8378108.jpeg?cs=srgb&dl=pexels-raquel-8378108.jpg&fm=jpg&w=640&h=960"
+            ),
+            0
+        ),
+        TileItem(
+            listOf(
+                "https://demo.sirv.com/nuphar.jpg?w=400",
+                "https://images.pexels.com/photos/8378108/pexels-photo-8378108.jpeg?cs=srgb&dl=pexels-raquel-8378108.jpg&fm=jpg&w=640&h=960"
+            ),
+            7000
+        ),
+    )
 }
+
+data class TileItem(
+    val list: List<String>,
+    val delay: Long
+)
